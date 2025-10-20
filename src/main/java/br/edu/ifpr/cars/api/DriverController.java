26fa1be807d53132f59b25dd6a5d5328578ecea1
isadora <isadora.izlou@gmail.com>
2025-10-20 08:17:57 -0300

    criando update

--- src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -3,12 +3,17 @@ package br.edu.ifpr.cars.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
@@ -28,6 +33,22 @@ public class DriverController {
 
     @GetMapping("/drivers/{id}")
     public Driver findDriver(@PathVariable Long id) {
-        return driverRepository.findById(id).get();
+        return driverRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @PostMapping("/drivers")
+    public Driver createDriver(@RequestBody Driver driver){
+        return driverRepository.save(driver);
+    }
+
+    @PutMapping("/drivers/{id}")
+    public Driver fullUpdateDriver(@PathVariable("id") Long id, @RequestBody Driver driver) {
+        
+        Driver foundDriver = findDriver(id);
+        foundDriver.setName(driver.getName());
+        foundDriver.setBirthDate(driver.getBirthDate());
+        return driverRepository.save(foundDriver);
     }
 }
